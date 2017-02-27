800aa0d61c2ec32d5673887e2502f0dccedba8b1
Pete <peefer1966@gmail.com>
2017-02-27 18:17:51 +0000

    Lecture 17

--- user/forms.py
+++ user/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import Form # documents => http://wtforms.readthedocs.io/en/lates
 from wtforms import validators, StringField, PasswordField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError
+import re
 
 #  Seperate the application level packages from the python level packages
 from user.models import User
@@ -41,6 +42,11 @@ class RegisterForm(Form):
     def validate_username(form, field):
         if User.objects.filter(username=field.data).first():
             raise ValidationError("Username already exists")
+            # Use regular expressions to validate username
+            # for acceptable characters and length
+        if not re.match("^[a-zA-Z0-9_-]{4,25}$", field.data):
+            raise ValidationError('Invalid username')
+        
             
     def validate_email(form, field):
         if User.objects.filter(email=field.data).first():
